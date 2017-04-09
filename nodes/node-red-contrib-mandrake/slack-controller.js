8d6a1a55f17d944fbf8b85bc1f7f77fd61b6b2b3
Siddhartha Lahiri <slahiri@sapient.com>
2017-04-09 16:19:27 +0530

    Slack channel node-red base modules. https://github.com/yantrashala/mandrake.ai/projects/1#card-2416683

--- nodes/node-red-contrib-mandrake/slack-controller.js
+++ nodes/node-red-contrib-mandrake/slack-controller.js
@@ -1,8 +1,7 @@
-'use strict';
-module.exports = function(RED) {
-    function SlackNode(n) {
-        RED.nodes.createNode(this,n);
-        this.bot_token = n.bot_token;
-    }
-    RED.nodes.registerType("slack-controller", SlackNode);
+module.exports = function (RED) {
+	function SlackNode(n) {
+		RED.nodes.createNode(this, n);
+		this.bot_token = n.bot_token;
+	}
+	RED.nodes.registerType("slack-controller", SlackNode);
 }
